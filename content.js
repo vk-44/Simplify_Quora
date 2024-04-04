84047c2188bdd4002ce898ff163ea82e0a7fd971
Vikram <vikramgreenliker44@gmail.com>
2024-04-04 17:00:05 +0530

    Code refactor and logic for removing related answers

--- content.js
+++ content.js
@@ -1,54 +1,67 @@
 //Removes ads
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  if (message.todo === "ads") {
-    document.getElementsByClassName("q-sticky").item(0).style.display = "none";
+  const adsElement = document.getElementsByClassName("q-sticky").item(0);
+
+  const promotedList = document.getElementsByClassName(
+    "spacing_log_question_page_ad"
+  );
+
+  const botElement = document
+    .getElementsByClassName(
+      "q-click-wrapper qu-display--block qu-tapHighlight--white qu-cursor--pointer ClickWrapper___StyledClickWrapperBox-zoqi4f-0 daLTSH"
+    )
+    .item(0);
+
+  const signInElement = document
+    .getElementsByClassName(
+      "q-flex qu-alignItems--center qu-justifyContent--center qu-overflow--hidden qu-zIndex--blocking_wall"
+    )
+    .item(0);
+
+  const overflowElement = document
+    .getElementsByClassName(
+      "q-platform--desktop q-color-mode--dark qu-color--gray_ultralight"
+    )
+    .item(0);
+
+  const relatedList = document.getElementsByClassName("q-box qu-mb--tiny");
+
+  if (message.todo === "ads" && adsElement) {
+    adsElement.style.display = "none";
   }
 
-  if (message.todo === "promoted") {
-    const promotedList = document.getElementsByClassName(
-      "spacing_log_question_page_ad"
-    );
-
+  if (message.todo === "promoted" && promotedList) {
     for (const item of promotedList) {
       item.style.display = "none";
     }
   }
 
-  if (message.todo === "bot") {
-    const botElement = document
-      .getElementsByClassName(
-        "q-click-wrapper qu-display--block qu-tapHighlight--white qu-cursor--pointer ClickWrapper___StyledClickWrapperBox-zoqi4f-0 daLTSH"
-      )
-      .item(0);
-
-    if (botElement) {
-      botElement.style.display = "none";
-    }
+  if (message.todo === "bot" && botElement) {
+    botElement.style.display = "none";
   }
 
-  if (message.todo === "sign") {
+  if (message.todo === "sign" && signInElement && overflowElement) {
     //Removing SignIn popup box and background blur
-    const signInElement = document
-      .getElementsByClassName(
-        "q-flex qu-alignItems--center qu-justifyContent--center qu-overflow--hidden qu-zIndex--blocking_wall"
-      )
-      .item(0);
-
-    if (signInElement) {
-      signInElement.style.display = "none";
-      signInElement.parentElement.previousElementSibling.style.filter = "";
-    }
+    signInElement.style.display = "none";
+    signInElement.parentElement.previousElementSibling.style.filter = "";
 
     //Removing overflow property
-    document
-      .getElementsByClassName(
-        "q-platform--desktop q-color-mode--dark qu-color--gray_ultralight"
-      )
-      .item(0).style.overflow = "";
+    overflowElement.style.overflow = "";
   }
 
-  if (message.todo === "related") {
-    const relatedList = document.getElementsByClassName("q-box qu-mb--tiny");
+  if (message.todo === "related" && relatedList) {
+    for (const item of relatedList) {
+      function findParent(element, className) {
+        if (!element || !element.classList) return null;
+        if (element.classList.contains(className)) {
+          return element;
+        } else {
+          return findParent(element.parentElement, className);
+        }
+      }
+      const parent = findParent(item, "qu-borderAll");
+      if (parent) parent.style.display = "none";
+    }
   }
 });
 
