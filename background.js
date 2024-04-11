8fc8d0957ecfa98eae3d5e938267af4fb4ba87a2
Vikram <vikramgreenliker44@gmail.com>
2024-04-11 13:43:46 +0530

    Setting Initial values for the checkbox

--- background.js
+++ background.js
@@ -1,6 +1,19 @@
+//Setting all the checkbox values to true when the extension is newly installed
+chrome.runtime.onInstalled.addListener(() => {
+  const checkboxValues = {
+    signin: true,
+    ads: true,
+    promoted: true,
+    related: true,
+  };
+
+  chrome.storage.local.set({ checkboxValues });
+});
+
 //Listen for messages from content scripts
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === "getData") {
+  //Returns the Object from chrome's local storage
+  if (request.action === "getObj") {
     chrome.storage.local
       .get(["checkboxValues"])
       .then((res) => sendResponse(res.checkboxValues));
@@ -9,7 +22,8 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     return true;
   }
 
-  if (request.action === "modifyUrl") {
+  //Modifies the URL with newURL
+  if (request.action === "modifyURL") {
     chrome.tabs.update(sender.tab.id, { url: request.newURL });
   }
 });
